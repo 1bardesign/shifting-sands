3c7e943747d70b9e6afd32cca762d23b0a5b8a5b
Max Cahill <1bardesign@gmail.com>
2019-11-08 13:26:35 +1100

    [added] separate vegetation layer

--- main.lua
+++ main.lua
@@ -7,7 +7,17 @@ local terrain_size = 200
 
 local chunky_pixels = 1
 
+function send_uniform_table(shader, t)
+	for k, v in pairs(t) do
+		if shader:hasUniform(k) then
+			shader:send(k, v)
+		end
+	end
+end
+
 function love.load()
+	first_draw = true
+
 	pixel = love.graphics.newCanvas(1, 1)
 
 	--screenbuffer
@@ -19,17 +29,7 @@ function love.load()
 	})
 	sbc:setFilter("nearest", "nearest")
 
-	--canvas for rendering terrain
-	terrain_canvas = love.graphics.newCanvas(terrain_res, terrain_res, {format="rgba16f"})
-
-	gradient_map = love.graphics.newImage("grad.png")
-	height_map = love.graphics.newImage("height.png")
-
-	shaders.world:send("detail_scale", detail_scale)
-
-	shaders.mesh:send("gradient_map", gradient_map)
-	shaders.mesh:send("height_map", height_map)
-
+	--terrain geom
 	local verts = {}
 	--generate corners
 	for y = 0, terrain_res do
@@ -41,7 +41,7 @@ function love.load()
 				(u - 0.5) * terrain_size,
 				(v - 0.5) * terrain_size,
 				--uvs
-				u, v
+				u, v,
 			})
 		end
 	end
@@ -65,32 +65,154 @@ function love.load()
 		end
 	end
 	--upload to gpu
-	terrain_mesh = love.graphics.newMesh( step_y * step_y, "triangles", "static")
+	terrain_mesh = love.graphics.newMesh( #verts, "triangles", "static")
 	terrain_mesh:setVertices(verts)
 	terrain_mesh:setVertexMap(indices)
+
+	--vegetation geom
+	local veg_verts = {}
+	local veg_indices = {}
+	for y = 1, terrain_res do
+		for x = 1, terrain_res do
+			local u = (x - 0.5) / terrain_res
+			local v = (y - 0.5) / terrain_res
+			local vx = (u - 0.5) * terrain_size
+			local vy = (v - 0.5) * terrain_size
+			--centered point
+			table.insert(veg_verts, {
+				vx,
+				vy,
+				--uvs
+				u, v,
+				--vert col as point flag
+				0.0
+			})
+			local i = #veg_verts
+			--corners
+			table.insert(veg_verts, {
+				vx - 0.5, vy - 0.5,
+				u, v,
+			})
+			table.insert(veg_verts, {
+				vx + 0.5, vy - 0.5,
+				u, v,
+			})
+			table.insert(veg_verts, {
+				vx + 0.5, vy + 0.5,
+				u, v,
+			})
+			table.insert(veg_verts, {
+				vx - 0.5, vy + 0.5,
+				u, v,
+			})
+
+			table.insert(veg_indices, i)
+			table.insert(veg_indices, i+1)
+			table.insert(veg_indices, i+2)
+
+			table.insert(veg_indices, i)
+			table.insert(veg_indices, i+2)
+			table.insert(veg_indices, i+3)
+
+			table.insert(veg_indices, i)
+			table.insert(veg_indices, i+3)
+			table.insert(veg_indices, i+4)
+
+			table.insert(veg_indices, i)
+			table.insert(veg_indices, i+4)
+			table.insert(veg_indices, i+1)
+		end
+	end
+
+	--
+	vegetation_mesh = love.graphics.newMesh( #veg_verts, "triangles", "static")
+	vegetation_mesh:setVertices(veg_verts)
+	vegetation_mesh:setVertexMap(veg_indices)
+
+	--canvas for rendering terrain
+	terrain_canvas = love.graphics.newCanvas(terrain_res, terrain_res, {format="rgba16f"})
+
+	gradient_map = love.graphics.newImage("grad.png")
+	height_map = love.graphics.newImage("height.png")
+
+	world_shader_uniforms = {
+		detail_scale = detail_scale,
+		seed_offset = {
+			(love.math.random() - 0.5) * 1000,
+			(love.math.random() - 0.5) * 1000,
+		},
+		t = love.math.random(),
+	}
+
+	send_uniform_table(shaders.world, world_shader_uniforms)
+
+	mesh_shader_uniforms = {
+		gradient_map = gradient_map,
+		height_map = height_map,
+		height_scale = 32,
+		terrain = terrain_canvas,
+		terrain_res = terrain_res,
+		obj_euler = {0, 0.0, math.pi * 0.5},
+		cam_offset = {0, terrain_size * -0.3, terrain_size * 0.7},
+		cam_euler = {0, 0, -math.pi * 0.5 * 0.25},
+
+		veg_vert_start = veg_offset,
+		veg_height_scale = 2,
+	}
+
+	for i,v in ipairs({
+		shaders.terrain_mesh,
+		shaders.vegetation_mesh,
+	}) do
+		send_uniform_table(v, mesh_shader_uniforms)
+	end
 end
 
-local t = love.math.random()
-local t_scale = (1 / 10000)
+local t_scale = (1 / 1000)
+
+function render_terrain()
+	love.graphics.setCanvas(terrain_canvas)
+	love.graphics.setBlendMode("alpha", "alphamultiply")
+	love.graphics.setShader(shaders.world)
+
+	local skip_draw = false
+
+	if first_draw then
+		love.graphics.setColor(1,1,1,1)
+	else
+		love.graphics.setColor(1,1,1, 0.01)
+		skip_draw = true
+	end
+
+	if not skip_draw then
+		love.graphics.draw(
+			pixel,
+			0, 0,
+			0,
+			terrain_res, terrain_res
+		)
+	end
+
+	love.graphics.setColor(1,1,1,1)
+end
 
 local cam_t = love.math.random() * math.pi * 2
 local cam_t_scale = (1 / 20) * math.pi * 2
 
 function love.draw()
-	--render to canvas
-	love.graphics.setCanvas(terrain_canvas)
-	
-	love.graphics.setBlendMode("alpha", "alphamultiply")
-	
-	love.graphics.setShader(shaders.world)
-	shaders.world:send("t", t)
+	--update shaders
+	send_uniform_table(shaders.world, world_shader_uniforms)
 
-	love.graphics.draw(
-		pixel,
-		0, 0,
-		0,
-		terrain_res, terrain_res
-	)
+	mesh_shader_uniforms.obj_euler[1] = cam_t
+	for i,v in ipairs{
+		shaders.terrain_mesh,
+		shaders.vegetation_mesh,
+	} do
+		send_uniform_table(v, mesh_shader_uniforms)
+	end
+
+	--render to canvas
+	render_terrain()
 	
 	--render mesh
 	love.graphics.setBlendMode("alpha", "alphamultiply")
@@ -99,23 +221,27 @@ function love.draw()
 		depth = true,
 	})
 	love.graphics.setDepthMode("greater", true)
+	
+	--clear to sea col (todo: lookup from tex)
 	local r = 0x1b / 255
 	local g = 0x30 / 255
 	local b = 0x99 / 255
 	love.graphics.clear(
-		r, g, b, 0,
+		r, g, b, 1.0,
 		0, 0
 	)
-	love.graphics.setShader(shaders.mesh)
-	shaders.mesh:send("terrain", terrain_canvas)
-	shaders.mesh:send("obj_euler", {cam_t, 0.0, math.pi * 0.5})
-	shaders.mesh:send("cam_offset", {0, terrain_size * -0.2, terrain_size * 0.5})
-	shaders.mesh:send("cam_euler", {0, 0, -0.5})
-	-- shaders.mesh:send("terrain_res", terrain_res)
-	shaders.mesh:send("height_scale", 24)
-	--render using mesh
+
+	love.graphics.setShader(shaders.terrain_mesh)
+	--render island
 	love.graphics.draw(
 		terrain_mesh,
+		cw * 0.5, ch * 0.5
+	)
+	--render vegetation
+	love.graphics.setShader(shaders.vegetation_mesh)
+	--render island
+	love.graphics.draw(
+		vegetation_mesh,
 		cw * 0.5, ch * 0.5
 	)
 
@@ -142,10 +268,12 @@ function love.draw()
 			tostring(last_dt)
 		))
 	end
+
+	first_draw = false
 end
 
 function love.update(dt)
-	t = t + dt * t_scale
+	world_shader_uniforms.t = world_shader_uniforms.t + dt * t_scale
 	cam_t = cam_t + dt * cam_t_scale 
 
 	last_dt = dt
@@ -153,9 +281,19 @@ end
 
 function love.keypressed(k)
 	local ctrl = love.keyboard.isDown("lctrl")
-	if k == "r" and ctrl then
-		love.event.quit("restart")
+	if k == "r" then
+		if ctrl then
+			love.event.quit("restart")
+		else
+			love.load()
+		end
 	elseif k == "q" and ctrl or k == "escape" then
 		love.event.quit()
+	elseif k == "s" and ctrl then
+		local id = sbc:newImageData()
+		love.filesystem.write(
+			string.format("screenshot-%d.png", os.time()),
+			id:encode("png")
+		)
 	end
 end
--- shaders.lua
+++ shaders.lua
@@ -1,148 +1,4 @@
-local world_shader = love.graphics.newShader([[
-extern float t;
-extern float detail_scale;
-
-#ifdef PIXEL
-vec3 hash3(vec2 p) {
-	vec3 q = vec3(dot(p, vec2(127.1, 311.7)),
-				  dot(p, vec2(269.5, 183.3)),
-				  dot(p, vec2(419.2, 371.9)));
-	return fract(sin(q) * 43758.5453);
-}
-
-vec3 point(vec2 x, float u) {
-	return (hash3(x) - vec3(0.5, 0.5, 0.0)) * vec3(u, u, 1.0) + vec3(x, 0.0);
-}
-
-float noise(vec2 x, float u, float v ) {
-	vec2 p = floor(x);
-	vec2 f = fract(x);
-
-	float vpow = 1.0 + 63.0 * pow(1.0 - v, 4.0);
-
-	float wv = 0.0;
-	float wt = 0.0;
-	for(int j = -2; j <= 2; j++)
-	for(int i = -2; i <= 2; i++)
-	{
-		vec2  g = vec2(float(i), float(j));
-		vec3  o = point(p + g, u);
-		vec2  r = o.xy - p - f;
-
-		float d = dot(r, r);
-		
-		d = sqrt(d);
-
-		float sfac = smoothstep(0.0, 1.0, clamp(d / 1.5, 0.0, 1.0));
-		float f = pow(1.0 - sfac, vpow);
-
-		wv += o.z * f;
-		wt += f;
-	}
-
-	return (wv / wt) * 2.0 - 1.0;
-}
-
-const float diminish_octave_scale = 0.5;
-float distorted_noise(vec2 x, int iters, float u, float v, vec2 vo1, vec2 vo2) {
-	float diminish_octave = float(iters) * diminish_octave_scale;
-	for(int i = 0; i < iters; i++) {
-		float fac = float(i + 1);
-		float scale = 1.0 / float(i + 1);
-		vec2 noff = vec2(
-			noise(
-				(x.xy + vo1 * fac),
-				u, v
-			),
-			noise(
-				(x.yx - vo1 * fac),
-				u, v
-			)
-		);
-		x += noff / fac * diminish_octave;
-	}
-	return noise(x, u, v);
-}
-
-vec2 rotate(vec2 x, float t) {
-	float s = sin(t);
-	float c = cos(t);
-	return vec2(
-		x.x * c + x.y * -s,
-		x.x * s + x.y * c
-	);
-}
-
-const float TAU = 3.14159;
-float fracnoise(vec2 p, float t, int distortions, int octaves) {
-	float n = 0.0;
-
-	for(int i = 0; i < octaves; i++) {
-		float f = pow(0.5, float(i + 1));
-		n += f * distorted_noise(
-			p * 0.1 * float(i + 1) + vec2(69.5, 13.3) * i,
-			distortions,
-			1.0, 1.0,
-			rotate(vec2(127.1, 311.7),  t * TAU + float(i)),
-			rotate(vec2(269.5, 183.3), -t * TAU + float(i))
-		);
-	}
-
-	return n;
-}
-
-vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords) {
-	vec2 pos = screen_coords * 0.25 * detail_scale;
-
-	float n = fracnoise(pos, t, 1, 8);
-
-	float dmid = length(texture_coords - vec2(0.5));
-
-	n = n + 1.0;
-	n -= dmid * 2.5;
-
-	float biome = 0.5 + distorted_noise(
-		pos * 0.071 + vec2(874.3, 57.1),
-		3,
-		1.0, 1.0,
-		rotate(vec2(127.1, 311.7), (t * 0.2) * TAU + 25.0),
-		rotate(vec2(269.5, 183.3), -(t * 0.2) * TAU)
-	) * 0.5;
-
-	float vegetation = float(distorted_noise(
-		pos * 0.43 + vec2(77.3, 99.7),
-		3,
-		1.0, 1.0,
-		rotate(vec2(127.1, 311.7), (t * 0.3) * TAU + 25.0),
-		rotate(vec2(269.5, 183.3), -(t * 0.3) * TAU)
-	) > 0.0 && n > 0.25 && n < 0.75);
-
-	float alpha = 0.01;
-
-	return vec4(
-		n,
-		biome,
-		vegetation,
-		alpha
-	);
-}
-#endif
-]])
-
-local mesh_shader = love.graphics.newShader([[
-extern Image terrain;
-
-extern Image gradient_map;
-extern Image height_map;
-
-extern float height_scale;
-
-extern vec3 cam_offset;
-extern vec3 cam_euler;
-
-extern vec3 obj_euler;
-
-const bool ortho = false;
+local shader_common_stuff = [[
 
 vec2 rotate(vec2 v, float t) {
 	float s = sin(t);
@@ -152,6 +8,16 @@ vec2 rotate(vec2 v, float t) {
 		v.x * s + v.y * c
 	);
 }
+
+]]
+
+local shader_3d_stuff = [[
+extern vec3 cam_offset;
+extern vec3 cam_euler;
+
+extern vec3 obj_euler;
+
+const bool ortho = false;
 
 vec3 rotate_euler(vec3 v, vec3 e) {
 	v.xy = rotate(v.xy, e.x);
@@ -163,7 +29,7 @@ vec3 rotate_euler(vec3 v, vec3 e) {
 mat4 proj(vec2 screen, float fov, float near, float far) {
 	float aspect = screen.x / screen.y;
 	float f = 1.0 / tan(fov / 2.0);
-	float xpr = aspect / f;
+	float xpr = f / aspect;
 	float ypr = f;
 	float fmn = (far - near);
 	float zpr = (far + near) / fmn;
@@ -193,18 +59,177 @@ vec4 transform_to_screen(vec4 v) {
 	}
 	else {
 		//perspective cam
-		v = proj(love_ScreenSize.xy, 1.0, 0.1, 1000.0) * v;
+		v = proj(love_ScreenSize.xy, 1.0, 1.0, 1000.0) * v;
 	}
 	return v;
 }
+]]
+
+local shader_noise_stuff = [[
+vec3 hash3(vec2 p) {
+	vec3 q = vec3(dot(p, vec2(127.1, 311.7)),
+				  dot(p, vec2(269.5, 183.3)),
+				  dot(p, vec2(419.2, 371.9)));
+	return fract(sin(q) * 43758.5453);
+}
+
+vec3 hash3_signed(vec2 p) {
+	return hash3(p) * 2.0 - vec3(1.0);
+}
+
+vec3 noise_point(vec2 x, float u) {
+	return (hash3(x) - vec3(0.5, 0.5, 0.0)) * vec3(u, u, 1.0) + vec3(x, 0.0);
+}
+
+float noise(vec2 x, float u, float v ) {
+	vec2 p = floor(x);
+	vec2 f = fract(x);
+
+	float vpow = 1.0 + 63.0 * pow(1.0 - v, 4.0);
+
+	float wv = 0.0;
+	float wt = 0.0;
+	for(int j = -2; j <= 2; j++)
+	for(int i = -2; i <= 2; i++)
+	{
+		vec2  g = vec2(float(i), float(j));
+		vec3  o = noise_point(p + g, u);
+		vec2  r = o.xy - p - f;
+
+		float d = dot(r, r);
+		
+		d = sqrt(d);
+
+		float sfac = smoothstep(0.0, 1.0, clamp(d / 1.5, 0.0, 1.0));
+		float f = pow(1.0 - sfac, vpow);
+
+		wv += o.z * f;
+		wt += f;
+	}
+
+	return (wv / wt) * 2.0 - 1.0;
+}
+
+const float TAU = 3.14159;
+float fracnoise(vec2 p, float t, float u, float v, int octaves, float octave_factor, float distort) {
+	float n = 0.0;
+
+	float i_of = 1.0 / octave_factor;
+	float octave_total = 0.0;
+
+	for(int i = 0; i < octaves; i++) {
+		float f = pow(octave_factor, float(i + 1));
+		octave_total += f;
+		float nval = noise(
+			rotate(
+				p,
+				t * TAU + float(i) * 0.71
+			),
+			u, v
+		);
+		n += f * nval;
+		//modify
+		p *= i_of;
+		p += rotate(
+			vec2(69.5, 13.3),
+			(float(i) * (nval * distort))
+		);
+	}
+
+	return n /= octave_total;
+}
+]]
+
+local world_shader = love.graphics.newShader(shader_common_stuff..shader_noise_stuff..[[
+extern float t;
+extern float detail_scale;
+extern vec2 seed_offset;
+
+#ifdef PIXEL
+vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords) {
+	vec2 pos = screen_coords * 0.05 * detail_scale + seed_offset;
+
+	//points for distance
+	float hd = 0.2;
+	vec3 h1 = hd * hash3_signed(seed_offset);
+	vec3 h2 = hd * hash3_signed(seed_offset + vec2(38.1, 91.3));
+	
+	vec2 p1 = h1.xy;
+	vec2 p2 = h2.xy;
+	vec2 p3 = vec2(h1.z, h2.x);
+	vec2 cent = (p1 + p2 + p3) / 3.0;
+	vec2 mid = vec2(0.5);
+	p1 = p1 - cent + mid;
+	p2 = p2 - cent + mid;
+	p3 = p3 - cent + mid;
+
+	float dmid = min(
+		min(
+			length(texture_coords - p1),
+			length(texture_coords - p2)
+		),
+		length(texture_coords - p3)
+	);
+
+	float n = fracnoise(
+		pos,
+		t, 1.0, 1.0,
+		4, 0.47,
+		0.1
+	);
+	n = 1.0 - abs(n) * 0.75;
+	n -= dmid * 3.0;
+
+	float biome = 0.5 + fracnoise(
+		pos * 0.51,
+		t, 1.0, 1.0,
+		1, 0.5,
+		0.0
+	) * 0.5;
+
+	float vn = fracnoise(
+		pos * 0.56 + vec2(84.3, 75.1),
+		t,
+		0.5, 1.0,
+		4, 0.7,
+		1.0
+	);
+	float vegetation = float(
+		//noise bound
+		abs(vn) < 0.25
+		//height bound
+		&& n > 0.4
+		&& n < 0.6
+	);
+
+	return vec4(
+		n,
+		biome,
+		vegetation,
+		color.a
+	);
+}
+#endif
+]])
+
+local shader_terrain_stuff = [[
+extern Image height_map;
+
+extern Image terrain;
+
+extern float height_scale;
 
 float height(vec2 uv) {
-	return Texel(height_map, uv).r;
+	return Texel(height_map, uv).r * height_scale;
 }
 
 float height_at(vec2 uv) {
 	return height(Texel(terrain, uv).xy);
 }
+]]
+
+local terrain_mesh_shader = love.graphics.newShader(shader_common_stuff..shader_3d_stuff..shader_terrain_stuff..[[
+extern Image gradient_map;
 
 #ifdef VERTEX
 vec4 position(mat4 transform_projection, vec4 vertex_position) {
@@ -215,19 +240,16 @@ vec4 position(mat4 transform_projection, vec4 vertex_position) {
 
 	float h = height(luv);
 
-	//apply noise offset
-	luv.x += t.z * 0.1;
-
+	//forward to frag for grad map lookup
 	VaryingTexCoord.xy = luv;
 
 	//project height
-	vertex_position.z = h * height_scale;
+	vertex_position.z = h;
 	
 	return transform_to_screen(vertex_position);
 }
 #endif
 #ifdef PIXEL
-vec3 light_dir = vec3(0.0, 0.0, 1.0);
 vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords) {
 	color.rgb = Texel(gradient_map, texture_coords).rgb;
 
@@ -236,7 +258,83 @@ vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords) {
 #endif
 ]])
 
+local vegetation_mesh_shader = love.graphics.newShader(shader_common_stuff..shader_noise_stuff..shader_3d_stuff..shader_terrain_stuff..[[
+extern Image gradient_map;
+
+extern float veg_height_scale;
+extern float terrain_res;
+
+#ifdef VERTEX
+vec4 position(mat4 transform_projection, vec4 vertex_position) {
+	vec2 uv = VaryingTexCoord.xy;
+	
+	//offset entire vegetation (including sample pos) by noise
+	vec2 voff = vec2(
+		noise(
+			uv * 128.0, 
+			1.0, 1.0
+		),
+		noise(
+			uv * 128.0 + vec2(4923.2, -230.1),
+			1.0, 1.0
+		)
+	);
+	uv += voff / terrain_res;
+	vertex_position.xy += voff;
+
+	//sample
+	vec4 t = Texel(terrain, uv);
+
+	vec2 luv = t.xy;
+	float veg_amount = t.z;
+
+	bool is_point = (VaryingColor.r == 0.0);
+	float point = float(is_point);
+
+	//write vert colour
+	
+	//pick native colour
+	VaryingColor.rgb = mix(
+		vec3(0.0, 0.2, 0.1),
+		vec3(0.1, 0.3, 0.0),
+		point
+	) + hash3(uv * 1000.0) * 0.05;
+	
+	//mix with underlying colour
+	VaryingColor = mix(
+		Texel(gradient_map, luv),
+		VaryingColor,
+		veg_amount * 0.25 + point * 0.5
+	);
+
+	if(veg_amount <= 0.0 && is_point) {
+		//clip
+		return vec4(0.0 / 0.0);
+	}
+
+	float h = 
+		height(luv)
+		+ (point * 2.0 - 1.0)
+			* veg_height_scale * (veg_amount * point);
+
+	//forward to frag for grad map lookup
+	VaryingTexCoord.xy = luv;
+
+	//project height
+	vertex_position.z = h;
+	
+	return transform_to_screen(vertex_position);
+}
+#endif
+#ifdef PIXEL
+vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords) {
+	return color;
+}
+#endif
+]])
+
 return {
 	world = world_shader,
-	mesh = mesh_shader,
+	terrain_mesh = terrain_mesh_shader,
+	vegetation_mesh = vegetation_mesh_shader,
 }
